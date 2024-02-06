7e92c426f17b7dc05cce843e92d20f2162dea695
willian ruiz <122157786+willianRuizCSS@users.noreply.github.com>
2024-02-06 15:41:08 -0500

    creando componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
-import { CommonModule } from '@angular/common';
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { GamesComponent } from './games/games.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule],
+  imports: [RouterOutlet,UserComponent,GamesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
